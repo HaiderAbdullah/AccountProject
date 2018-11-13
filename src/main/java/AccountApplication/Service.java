c3baf4ef077050af32361cb73ef5cd5e7a071007
Abdullah Haider <haider.abdullah14@gmail.com>
2018-11-13 13:43:14 +0000

    Third Commit

--- src/main/java/AccountApplication/Service.java
+++ src/main/java/AccountApplication/Service.java
@@ -1,7 +1,13 @@
 package AccountApplication;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 public class Service {
@@ -11,7 +17,7 @@ public class Service {
 	public void createAccount() {
 	
 	Account account = new Account("Abdullah", "Haider", 123);
-	System.out.println("est");
+	System.out.println("");
 	
 	Map<String, Account> numberMapping = new HashMap<String, Account>();
 
@@ -20,6 +26,51 @@ public class Service {
 	numberMapping.put("1", new Account(account.getFirstName(),account.getLastName(),account.getAccountNumber()));
 
 	System.out.println(numberMapping.values());
+	
+	
+	Service obj = new Service();
+
+	obj.run();
+
+}
+	
+	private void run() {
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		Account account = createObject();
+
+		
+
+		try {
+
+			mapper.writeValue(new File("C:\\Users\\Admin\\eclipse-workspace\\AccountProject\\Account.json"), account);
+
+			
+
+			String jsonInString = mapper.writeValueAsString(account);
+
+			System.out.println(jsonInString);
+
+			
+
+			jsonInString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(account);
+
+			System.out.println(jsonInString);
+
+		} catch (JsonGenerationException e) {
+
+			e.printStackTrace();
+
+		} catch (JsonMappingException e) {
+
+			e.printStackTrace();
+
+	} catch (IOException e) {
+
+		e.printStackTrace();
+
+	}
 
 }
 
